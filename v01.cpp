94b3de714d4b0e1553bf2edc2ce9e905c2e32cd0
karal1us <43696396+GalvosasJ@users.noreply.github.com>
2025-09-18 21:29:48 +0300

    big change menu

--- v01.cpp
+++ v01.cpp
@@ -2,6 +2,9 @@
 #include <vector>
 #include <iomanip>
 #include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <string>
 using namespace std;
 
 struct Studentas {
@@ -32,71 +35,108 @@ double mediana(vector<int> nd) {
 
 
 int main() {
+    srand(time(0)); 
     vector<Studentas> studentai;
-    char pasirinkimas;
+    char meniu_pasirinkimas;
+    char skaiciavimo_pasirinkimas;
     char testi = 't';
 
-    while (testi == 't') {
-        Studentas studentas;
-        
-        cout << "Iveskite studento varda ir pavarde: ";
-        cin >> studentas.vardas >> studentas.pavarde;
+    while (true) {
+        cout << endl << "MENU" << endl;
+        cout << "1. Sarasas" << endl;
+        cout << "2. Prideti studenta" << endl;
+        cout << "3. Stabdyti programa" << endl;
+        cout << "Pasirinkite veiksma (1-3): ";
+        cin >> meniu_pasirinkimas;
 
-        cout << "Iveskite namu darbu pazymius (baigti - du kartus ENTER): ";
-        string eilute;
-        getline(cin >> ws, eilute);
+        if (meniu_pasirinkimas == '1') {
+            if (studentai.empty()) {
+                cout << "Studentu sarasas tuscias!" << endl;
+                continue;
+            }
+            
+            cout << "Pasirinkite skaiciavimo metoda (v - vidurkis, m - mediana, a - abu): ";
+            cin >> skaiciavimo_pasirinkimas;
+            
+            if (skaiciavimo_pasirinkimas == 'v') {
+                cout << "Vardas" << setw(15) << "Pavarde" << setw(35) << "Galutinis (Vid.)" << endl;
+                cout << "--------------------------------------------------------------" << endl;
+                for (const auto& studentas : studentai) {
+                    double galutinis_vid = 0.4 * vidurkis(studentas.nd) + 0.6 * studentas.egzaminas;
+                    cout << fixed << setprecision(2) << studentas.vardas << setw(15) << studentas.pavarde << setw(15) << galutinis_vid << endl;
+                }
+            } else if (skaiciavimo_pasirinkimas == 'm') {
+                cout << "Vardas" << setw(15) << "Pavarde" << setw(35) << "Galutinis (Med.)" << endl;
+                cout << "--------------------------------------------------------------" << endl;
+                for (const auto& studentas : studentai) {
+                    double galutinis_med = 0.4 * mediana(studentas.nd) + 0.6 * studentas.egzaminas;
+                    cout << fixed << setprecision(2) << studentas.vardas << setw(15) << studentas.pavarde << setw(15) << galutinis_med << endl;
+                }
+            } else if (skaiciavimo_pasirinkimas == 'a') {
+                cout << "Vardas" << setw(15) << "Pavarde" << setw(20) << "Galutinis (Vid.)" << setw(20) << "Galutinis (Med.)" << endl;
+                cout << "----------------------------------------------------------------" << endl;
+                for (const auto& studentas : studentai) {
+                    double galutinis_vid = 0.4 * vidurkis(studentas.nd) + 0.6 * studentas.egzaminas;
+                    double galutinis_med = 0.4 * mediana(studentas.nd) + 0.6 * studentas.egzaminas;
+                    cout << fixed << setprecision(2) << studentas.vardas << setw(15) << studentas.pavarde << setw(15) << galutinis_vid << setw(15) << galutinis_med << endl;
+                }
+            } else {
+                cout << "Neteisingas pasirinkimas! Naudojamas vidurkis." << endl;
+                cout << "Vardas" << setw(15) << "Pavarde" << setw(35) << "Galutinis (Vid.)" << endl;
+                cout << "--------------------------------------------------------------" << endl;
+                for (const auto& studentas : studentai) {
+                    double galutinis_vid = 0.4 * vidurkis(studentas.nd) + 0.6 * studentas.egzaminas;
+                    cout << fixed << setprecision(2) << studentas.vardas << setw(15) << studentas.pavarde << setw(15) << galutinis_vid << endl;
+                }
+            }
+        } else if (meniu_pasirinkimas == '2') {
+            while (testi == 't') {
+                Studentas studentas;
+                
+                cout << "Iveskite studento varda ir pavarde: ";
+                cin >> studentas.vardas >> studentas.pavarde;
 
-        while (!eilute.empty()) {
-            int pazymys = stoi(eilute);
-            studentas.nd.push_back(pazymys);
-            getline(cin, eilute);
+                char generuoti;
+                cout << "Ar norite generuoti pazymius atsitiktinai? (t - taip, n - ne): ";
+                cin >> generuoti;
+
+                if (generuoti == 't') {
+                    int k = 20;
+                    int nd_skaicius = rand() % k + 1; 
+                    
+                    for (int i = 0; i < nd_skaicius; i++) {
+                        studentas.nd.push_back(rand() % 10 + 1);
+                    }
+                    
+                    studentas.egzaminas = rand() % 10 + 1;
+                    cout << "Sugeneruota " << nd_skaicius << " namu darbu pazymiu ir egzamino pazymys!" << endl;
+                } else {
+                    cout << "Iveskite namu darbu pazymius (baigti - du kartus ENTER): ";
+                    string eilute;
+                    getline(cin >> ws, eilute);
+
+                    while (!eilute.empty()) {
+                        int pazymys = stoi(eilute);
+                        studentas.nd.push_back(pazymys);
+                        getline(cin, eilute);
+                    }
+                    
+                    cout << "Iveskite egzamino pazymi: ";
+                    cin >> studentas.egzaminas;
+                }
+                
+                studentai.push_back(studentas);
+                cout << "Ar norite ivesti kita studenta? (t - taip, n - ne): ";
+                cin >> testi;
+            }
+            testi = 't';
+        } else if (meniu_pasirinkimas == '3') {
+            cout << "Programa stabdoma..." << endl;
+            break;
+        } else {
+            cout << "Neteisingas pasirinkimas! Bandykite dar kartą." << endl;
         }
-        
-        cout << "Iveskite egzamino pazymi: ";
-        cin >> studentas.egzaminas;
-        
-        studentai.push_back(studentas);
-        
-        cout << "Ar norite ivesti kita studenta? (t - taip, n - ne): ";
-        cin >> testi;
     }
-
-    cout << "Pasirinkite skaiciavimo metoda (v - vidurkis, m - mediana, a - abu): ";
-    cin >> pasirinkimas;
-
-    if (pasirinkimas == 'v') {
-        cout << "Vardas" << setw(15) << "Pavarde" << setw(35) << "Galutinis (Vid.)" << endl;
-        cout << "--------------------------------------------------------------" << endl;
-        for (const auto& studentas : studentai) {
-            double galutinis_vid = 0.4 * vidurkis(studentas.nd) + 0.6 * studentas.egzaminas;
-            cout << fixed << setprecision(2) << studentas.vardas << setw(15) << studentas.pavarde << setw(15) << galutinis_vid << endl;
-        }
-    } else if (pasirinkimas == 'm') {
-        cout << "Vardas" << setw(15) << "Pavarde" << setw(35) << "Galutinis (Med.)" << endl;
-        cout << "--------------------------------------------------------------" << endl;
-        for (const auto& studentas : studentai) {
-            double galutinis_med = 0.4 * mediana(studentas.nd) + 0.6 * studentas.egzaminas;
-            cout << fixed << setprecision(2) << studentas.vardas << setw(15) << studentas.pavarde << setw(15) << galutinis_med << endl;
-        }
-    } else if (pasirinkimas == 'a') {
-        cout << "Vardas" << setw(15) << "Pavarde" << setw(20) << "Galutinis (Vid.)" << setw(20) << "Galutinis (Med.)" << endl;
-        cout << "----------------------------------------------------------------" << endl;
-        for (const auto& studentas : studentai) {
-            double galutinis_vid = 0.4 * vidurkis(studentas.nd) + 0.6 * studentas.egzaminas;
-            double galutinis_med = 0.4 * mediana(studentas.nd) + 0.6 * studentas.egzaminas;
-            cout << fixed << setprecision(2) << studentas.vardas << setw(15) << studentas.pavarde << setw(15) << galutinis_vid << setw(15) << galutinis_med << endl;
-        }
-    } else {
-        cout << "Neteisingas pasirinkimas! Naudojamas vidurkis." << endl;
-        cout << "Vardas" << setw(15) << "Pavarde" << setw(35) << "Galutinis (Vid.)" << endl;
-        cout << "--------------------------------------------------------------" << endl;
-        for (const auto& studentas : studentai) {
-            double galutinis_vid = 0.4 * vidurkis(studentas.nd) + 0.6 * studentas.egzaminas;
-            cout << fixed << setprecision(2) << studentas.vardas << setw(15) << studentas.pavarde << setw(15) << galutinis_vid << endl;
-        }
-    }
-
-    
 
     return 0;
 }
